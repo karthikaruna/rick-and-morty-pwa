16853918d6cb40ddae3d8cee0efdca0e11c7ccde
Karthikeyan Arumugam <karthikaruna@live.com>
2020-10-26 12:11:22 +0530

    Get rid of mirage

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import { makeServer } from './server'
 
 Vue.config.productionTip = false
-
-if (process.env.NODE_ENV === 'test') {
-  makeServer()
-}
 
 new Vue({
   router,
