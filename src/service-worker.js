cbe354a41afc2e2b1b8654b533a899a003a425e0
karthikaruna1994 <73379864+karthikaruna1994@users.noreply.github.com>
2020-10-28 00:04:46 +0530

    Task 2 (#2)
    * Add index route - locations
    
    * Fetch residents of a location on click
    
    * Handle empty states
    
    * Add character details page
    
    * Introduce network middleware
    
    * Introduce mirage for API mocking in tests
    
    * Fix mirage interception
    
    * Ditch Mirage's ORM
    
    * Get rid of mirage
    
    * Add our own fetch-interceptor
    
    * Add component tests
    
    * Add e2e tests
    
    * Filter out Alive or Dead chars only
    
    * Move test-data inside /tests
    
    * PWA
    
    * Respond with index.html on navigation
    
    * Cache images in IDB as blob
    
    * Fix await operator's precedency
    
    * Image caching not possible; remove attempting code
    
    * Stop image requests from passing through the cache machinery

---
+++ src/service-worker.js
@@ -0,0 +1,125 @@
+/* global workbox */
+workbox.core.setCacheNameDetails({ prefix: 'lb-trial' })
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
+
+workbox.routing.registerNavigationRoute('/index.html')
+
+const version = 1
+const IDBConfig = {
+  name: 'lb_trial',
+  version,
+  stores: [
+    {
+      name: 'api_responses',
+      keyPath: 'url'
+    }
+  ]
+}
+
+const createIndexedDB = ({ name, version, stores }) => {
+  const request = self.indexedDB.open(name, version)
+
+  return new Promise((resolve, reject) => {
+    request.onupgradeneeded = e => {
+      const db = e.target.result
+
+      stores.map(({ name, keyPath }) => {
+        if (!db.objectStoreNames.contains(name)) {
+          db.createObjectStore(name, { keyPath })
+        }
+      })
+    }
+
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
+}
+
+const getStoreFactory = (dbName, version) => ({ name }, mode = 'readonly') => {
+  return new Promise((resolve, reject) => {
+    const request = self.indexedDB.open(dbName, version)
+
+    request.onsuccess = e => {
+      const db = request.result
+      const transaction = db.transaction(name, mode)
+      const store = transaction.objectStore(name)
+
+      return resolve(store)
+    }
+
+    request.onerror = e => reject(request.error)
+  })
+}
+
+const openStore = getStoreFactory(IDBConfig.name, version)
+
+const cacheApiResponse = async response => {
+  try {
+    const store = await openStore(IDBConfig.stores[0], 'readwrite')
+
+    store.add(response)
+  } catch (error) {
+    console.log('idb error', error)
+  }
+}
+
+const getCachedApiResponse = request => {
+  return new Promise((resolve, reject) => {
+    openStore(IDBConfig.stores[0])
+      .then(store => {
+        const cachedRequest = store.get(request.url)
+
+        cachedRequest.onsuccess = e => {
+          return cachedRequest.result === undefined ? resolve(null) : resolve(new Response(JSON.stringify(cachedRequest.result.json)))
+        }
+
+        cachedRequest.onerror = e => {
+          console.log('cached response error', e, cachedRequest.error)
+
+          return reject(cachedRequest.error)
+        }
+      })
+  })
+}
+
+const networkThenCache = async request => {
+  const { method, url } = request
+
+  try {
+    const response = await fetch(request)
+    const json = await response.clone().json()
+
+    if (method === 'GET') {
+      cacheApiResponse({ url, json })
+    }
+
+    return response
+  } catch (e) {
+    return new Response(JSON.stringify({ message: 'no response' }))
+  }
+}
+
+const getCachedOrNetworkApiResponse = async request => await getCachedApiResponse(request) || networkThenCache(request)
+
+const activateHandler = e => {
+  if (self.indexedDB) {
+    createIndexedDB(IDBConfig)
+  }
+}
+
+const fetchHandler = async e => {
+  const { request } = e
+  const { url, destination } = request
+
+  // images can't be cached because of response type opaque
+  if (url.includes('/api') && destination !== 'image') {
+    e.respondWith(getCachedOrNetworkApiResponse(request))
+  }
+}
+
+self.addEventListener('activate', activateHandler)
+self.addEventListener('fetch', fetchHandler)
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
